486a233cb2ac5f463cb7dad239cdc0e0d00db883
Badr AlSayed <badrmohamed1000@gmail.com>
2023-09-20 20:13:42 +0300

    feat: display home page tickers cards using react query, and display laoding cards when tickers query is loading

--- app/(stocks)/index.tsx
+++ app/(stocks)/index.tsx
@@ -1,34 +1,79 @@
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, FlatList } from 'react-native'
+import React from 'react'
+// import EditScreenInfo from '../../components/EditScreenInfo'
+import { View, Text } from '../../components/Themed'
+import LoadingCard from '../../components/LoadingCard'
+import SearchBar from '../../components/SearchBar'
+import { getTickers } from '../../api/Tickers'
+import { useQuery } from '@tanstack/react-query'
+import TickerCard from '../../components/TickerCard'
 
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
+export default function Home(): React.ReactElement {
+  const tickersQuery = useQuery({
+    queryKey: ['tickers'],
+    queryFn: getTickers
+  })
 
-export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <SearchBar />
+      {tickersQuery.isLoading ? (
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.row}>
+            <LoadingCard />
+            <LoadingCard />
+          </View>
+          <View style={styles.row}>
+            <LoadingCard />
+            <LoadingCard />
+          </View>
+          <View style={styles.row}>
+            <LoadingCard />
+            <LoadingCard />
+          </View>
+          <View style={styles.row}>
+            <LoadingCard />
+            <LoadingCard />
+          </View>
+          <View style={styles.row}>
+            <LoadingCard />
+            <LoadingCard />
+          </View>
+        </ScrollView>
+      ) : tickersQuery.isError ? (
+        <Text>Error</Text>
+      ) : (
+        <FlatList
+          data={tickersQuery.data.results}
+          numColumns={2}
+          keyExtractor={(item) => item.ticker}
+          renderItem={({ item }) => <TickerCard data={item} />}
+        />
+      )}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%'
   },
-});
+  row: {
+    flexDirection: 'row'
+  },
+  card: {
+    width: 165,
+    height: 131
+  }
+})
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,9 @@ import { useFonts } from 'expo-font'
 import { SplashScreen, Stack } from 'expo-router'
 import React, { useEffect } from 'react'
 import { useColorScheme, Image } from 'react-native'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -53,24 +56,26 @@ function RootLayoutNav(): React.ReactElement {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen
-          name='(stocks)'
-          options={{
-            headerTitle: '',
-            headerLeft: () => (
-              <Image
-                source={require('../assets/images/NasdaqLogo.png')}
-                resizeMode='contain'
-              />
-            ),
-            headerStyle: {
-              backgroundColor: '#191A28'
-            }
-          }}
-        />
-        <Stack.Screen name='modal' options={{ presentation: 'modal' }} />
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Screen
+            name='(stocks)'
+            options={{
+              headerTitle: '',
+              headerLeft: () => (
+                <Image
+                  source={require('../assets/images/NasdaqLogo.png')}
+                  resizeMode='contain'
+                />
+              ),
+              headerStyle: {
+                backgroundColor: '#191A28'
+              }
+            }}
+          />
+          <Stack.Screen name='modal' options={{ presentation: 'modal' }} />
+        </Stack>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
