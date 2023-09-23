61900add83314ff27ed3e812650ea1f1c6618d36
Badr AlSayed <badrmohamed1000@gmail.com>
2023-09-23 13:08:01 +0300

    feat: add a button link to close bottom sheet

--- components/BottomSheet/Header.tsx
+++ components/BottomSheet/Header.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Image } from 'react-native'
 import { Text, View } from '../Themed'
 import { type UseQueryResult } from '@tanstack/react-query'
 import type { PreviousClose, TickerDetails } from '../../models/model'
+import { Link } from 'expo-router'
 
 interface HeaderProps {
   tickerDetailsQuery: UseQueryResult<TickerDetails, undefined>
@@ -32,7 +33,7 @@ const Header: React.FC<HeaderProps> = ({
 
   return (
     <View style={styles.container}>
-      <View style={styles.firstLeft}>
+      <View style={styles.headerData}>
         {tickerDetails?.branding?.icon_url === undefined ? (
           <>
             <View style={styles.logo}>
@@ -56,23 +57,23 @@ const Header: React.FC<HeaderProps> = ({
         )}
 
         <View>
-          <Text>{(tickerPrevCloseQuery.data as PreviousClose)?.ticker}</Text>
+          <Text>{(tickerPrevCloseQuery.data as PreviousClose).ticker}</Text>
           <View style={{ flexDirection: 'row' }}>
             <Text style={{ paddingRight: 3 }}>
-              {`$${(
-                tickerPrevCloseQuery.data as PreviousClose
-              )?.results[0]?.c?.toFixed(2)}`}
+              {`$${tickerPrevClose.c.toFixed(2)}`}
             </Text>
-            <Text
-              style={priceChange(tickerPrevClose?.c, tickerPrevClose?.o)[1]}
-            >
-              {priceChange(tickerPrevClose?.c, tickerPrevClose?.o)[0]}
+            <Text style={priceChange(tickerPrevClose.c, tickerPrevClose.o)[1]}>
+              {priceChange(tickerPrevClose.c, tickerPrevClose.o)[0]}
             </Text>
           </View>
         </View>
       </View>
-      <View style={styles.firstRight}>
-        <Text>Cancel</Text>
+      <View>
+        <Link href='../'>
+          <View style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>x</Text>
+          </View>
+        </Link>
       </View>
     </View>
   )
@@ -95,40 +96,11 @@ const styles = StyleSheet.create({
     width: 350,
     height: 66
   },
-  firstLeft: {
+  headerData: {
     flexDirection: 'row',
     alignItems: 'flex-end',
     justifyContent: 'space-around',
     width: 150
-  },
-  firstRight: {
-    // flexDirection: 'row'
-  },
-  initials: {
-    width: 50,
-    height: 20,
-    borderRadius: 5,
-    backgroundColor: '#2C2E45',
-    marginVertical: 10,
-    borderWidth: 0.5,
-    borderColor: '#323443'
-  },
-  name: {
-    width: 30,
-    height: 16,
-    borderRadius: 5,
-    backgroundColor: '#2C2E45',
-    borderWidth: 0.5,
-    borderColor: '#323443'
-  },
-  logo1: {
-    width: 45,
-    height: 45,
-    backgroundColor: '#2C2E45',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#323443',
-    justifyContent: 'center'
   },
   logoInitials: {
     color: '#FFF',
@@ -146,6 +118,26 @@ const styles = StyleSheet.create({
   },
   zero: {
     color: 'grey'
+  },
+  closeButton: {
+    borderRadius: 50,
+    borderColor: '#323443',
+    borderWidth: 1,
+    backgroundColor: '#242639',
+    padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  closeButtonText: {
+    paddingHorizontal: 8,
+    fontSize: 20
+  },
+  closee: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    backgroundColor: '#242639'
   }
 })
 
