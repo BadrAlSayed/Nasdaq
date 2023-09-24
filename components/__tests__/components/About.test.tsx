eedd6427fd0751817249325953865de07e212fe7
Badr AlSayed <badrmohamed1000@gmail.com>
2023-09-24 16:21:11 +0300

    test: add unit tests to bottom sheet components

---
+++ components/__tests__/components/About.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react-native'
+import React from 'react'
+import About from '../../BottomSheet/About'
+
+describe('About', () => {
+  it('should render the correct ticker description', () => {
+    render(<About tickerDescription='Hello test' />)
+    expect(screen.getByText('Hello test')).toBeTruthy()
+  })
+})
